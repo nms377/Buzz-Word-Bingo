0243344ed94b83756d47b3f64ad5058fce4bff0e
nms377 <nms377@nyu.edu>
2017-01-04 21:59:21 -1000

    Created a get method that sends a json response when the uri /buzzword is requested.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
 
 app.get('/', express.static('public'));
+
+app.get('/buzzword', function (req, res, next) {
+	res.json();
+});
 
 var server = app.listen(3000, () => {
 	var host = server.address().address;
