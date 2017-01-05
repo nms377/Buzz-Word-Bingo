39c0521ff9bc249ccc32b295f8894dc6a6a49b67
nms377 <nms377@nyu.edu>
2017-01-04 21:09:39 -1000

    Created app.get('/') that servers the index.html file in the public directory. Curled localhost:3000 and got the test response Hello World.

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 var app = express();
 
+//GET / shoould serve the index.html file in the public directory
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+
 var server = app.listen(3000, () => {
 	var host = server.address().address;
 	var port = server.address().port;
