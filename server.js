90fcd7c9c1a097f672bfed5919d742252db5d189
nms377 <nms377@nyu.edu>
2017-01-04 22:21:13 -1000

    created a GET to /buzzword and created an app.use for body parser to parse application/x-www-form-urlencoded.

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', express.static('public'));
 
 app.get('/buzzword', function (req, res, next) {
-	res.json();
+	res.send({ "buzzword": buzzword(Array)});
 });
 
 var server = app.listen(3000, () => {
