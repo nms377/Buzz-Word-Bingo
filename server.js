25ccd7c5fb13c45f3cb264483e35652cf058ae5a
nms377 <nms377@nyu.edu>
2017-01-04 21:00:48 -1000

    Created the server.js file, ran the file in node and server is listening.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var app = express();
+
+var server = app.listen(3000, () => {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Server listening at http://%s%s', host, port);
+});
+
